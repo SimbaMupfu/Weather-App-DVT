10d6d10c0f9404c6e538ea57b90695d077da0f1b
simba_mupfu <scmupfururirwa@gmail.com>
2024-07-15 06:56:43 +0200

    Setup the network module with Hilt.

---
+++ app/src/main/java/inc/sims/hustles/weatherappdvt/utils/Constants.kt
@@ -0,0 +1,9 @@
+package inc.sims.hustles.weatherappdvt.utils
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.openweathermap.org"
+        const val API_KEY = "c886cf9bd5976bc24a1b77274aad6917"
+    }
+}
